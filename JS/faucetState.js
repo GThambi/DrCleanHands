96ee849080bd7ae6481196914c00d7be0052c0cb
GThambi <gopi@gtrdesign.ca>
2016-12-05 19:14:23 -0500

    Update faucetState.js

--- JS/faucetState.js
+++ JS/faucetState.js
@@ -2,7 +2,7 @@ var Firebase = require("firebase");
 var myFirebaseRef = new Firebase("https://drch-eef35.firebaseio.com/csi");
  
  
-myFirebaseRef.child("status").on("value", function(snapshot){            // probably will need to get the right firbaase child reference for status
+myFirebaseRef.child("mod").on("value", function(snapshot){            // probably will need to get the right firbaase child reference for status
 //myFirebaseRef.on("status", function(snapshot){
  
     var faucetState = snapshot.val()
